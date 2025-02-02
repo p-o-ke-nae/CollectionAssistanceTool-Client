e6dd8a8f5cdc2c3ad369251a4099ad305e8d84c2
oza <126060859+oza-tool@users.noreply.github.com>
2025-02-02 15:45:52 +0900

    devtest

--- src/app/Collection.jsx
+++ src/app/Collection.jsx
@@ -15,7 +15,7 @@ const Collection = ({ showInfo }) => {
             height={38}
             priority
           />
-              <CustomButton onClick={() => { showInfo("デプロイしましたdev", 1); }}>test</CustomButton>
+              <CustomButton onClick={() => { showInfo("デプロイしましたdevテスト", 1); }}>test</CustomButton>
           <ol>
             <li>
               Get started by editing <code>src/app/page.tsx</code>.
